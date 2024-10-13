4880b2f798f822325b75f36edb9141a1aacff5ab
John Chen <john_chen@outlook.com>
2024-10-12 19:30:59 -0700

    Update ch12/methods with new time.Duration methos
    As of Go version 1.22.6.

--- ch12/methods/methods_test.go
+++ ch12/methods/methods_test.go
@@ -14,11 +14,16 @@ func ExamplePrintDuration() {
 	methods.Print(time.Hour)
 	// Output:
 	// type time.Duration
+	// func (time.Duration) Abs() time.Duration
 	// func (time.Duration) Hours() float64
+	// func (time.Duration) Microseconds() int64
+	// func (time.Duration) Milliseconds() int64
 	// func (time.Duration) Minutes() float64
 	// func (time.Duration) Nanoseconds() int64
+	// func (time.Duration) Round(time.Duration) time.Duration
 	// func (time.Duration) Seconds() float64
 	// func (time.Duration) String() string
+	// func (time.Duration) Truncate(time.Duration) time.Duration
 }
 
 func ExamplePrintReplacer() {
@@ -34,11 +39,16 @@ func ExamplePrintReplacer() {
 methods.Print(time.Hour)
 // Output:
 // type time.Duration
+// func (time.Duration) Abs() time.Duration
 // func (time.Duration) Hours() float64
+// func (time.Duration) Microseconds() int64
+// func (time.Duration) Milliseconds() int64
 // func (time.Duration) Minutes() float64
 // func (time.Duration) Nanoseconds() int64
+// func (time.Duration) Round(time.Duration) time.Duration
 // func (time.Duration) Seconds() float64
 // func (time.Duration) String() string
+// func (time.Duration) Truncate(time.Duration) time.Duration
 
 methods.Print(new(strings.Replacer))
 // Output:
